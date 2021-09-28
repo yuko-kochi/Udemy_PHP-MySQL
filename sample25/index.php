8e75fb042eed25c0d6ddbdd0b145032f7996a26f
YukoKochi <happy.k165131@gmail.com>
2021-09-28 19:28:15 +0900

    [Finish]38_セッションに値を保存

---
+++ sample25/index.php
@@ -0,0 +1,33 @@
+<?php
+  // セッションはブラウザを閉じたらidが消滅する
+  // session_start(); を記述しないとphpでは$_SESSIONが使用できない
+  session_start();
+  $_SESSION['session_massage'] = '値をセッションに保存しました';
+?>
+
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="../css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+  <p>セッションに値を保存しました。次のページに移動してみましょう。</p>
+  <a href="page02.php">Page02へ</a>
+</pre>
+</main>
+</body>
+</html>
