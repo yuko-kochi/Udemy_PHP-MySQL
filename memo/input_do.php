adc9c684cf69de4c95684ca5e27732f9be65b80a
YukoKochi <happy.k165131@gmail.com>
2021-10-08 15:52:14 +0900

    [Finish]61_formからデータを保存する

--- memo/input_do.php
+++ memo/input_do.php
@@ -22,11 +22,21 @@
   try {
     $db = new PDO('mysql:dbname=mydb;host=localhost;charset=utf8','root','root');
 
+    // prepareは安全な構文
+    // prepareはmemo=?の?の部分にexecute(array($_POST['memo'])で指定した値が入る
+    $statement = $db->prepare('INSERT INTO memos SET memo=?, created_at=NOW()');
+    $statement->execute(array($_POST['memo']));
+    // 登録した際に画面にメッセージがでるように記述
+    echo 'メッセージが登録されました';
+
+
     // INSERT構文を使用する場合はexecを使う
     // exec('この中にSQL分を記述')
     // $_POST['memo']はinput.htmlのformで指定したmethod属性
     // $_POST['memo']のmemoの部分は textarea nameで指定した名前
-    $db->exec('INSERT INTO memos SET memo="' . $_POST['memo'] . '", created_at=NOW()');
+    // 下記の構文だと$_POST['memo']の値がそのまま指定されているため、危険
+    // $_POST['memo']をそのまま渡してしまうと、危険な文字列や記号など入っているとSQLが壊されて、dbにアクセスされる可能性がある
+    // $db->exec('INSERT INTO memos SET memo="' . $_POST['memo'] . '", created_at=NOW()');
 
   } catch(PDOException $e) {
     echo 'DB接続エラー: ' . $e->getMessage();
