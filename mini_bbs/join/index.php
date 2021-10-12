d20c043b9957f8ed4d149d489c2fc9bc8a40c227
YukoKochi <happy.k165131@gmail.com>
2021-10-12 16:39:04 +0900

    [Finish]73_エラーチェック

--- mini_bbs/join/index.php
+++ mini_bbs/join/index.php
@@ -1,7 +1,27 @@
 <?php
+// !empty($_POST)で　form が送信されているかどうか、空ではない時に命令
+if (!empty($_POST)) {
 	if ($_POST['name'] === ''){
 		$error['name'] = 'blank';
 	}
+	if ($_POST['email'] === ''){
+		$error['email'] = 'blank';
+	}
+	// strlen は指定された文字数を測って数字で返す
+	if (strlen($_POST['password']) < 4){
+		$error['password'] = 'length';
+	}
+	if ($_POST['password'] === ''){
+		$error['password'] = 'blank';
+	}
+
+	// empty() は配列の中が空であるか判断する
+	if (empty($error)) {
+		// check.php にジャンプする命令
+		header('Location: check.php');
+		exit();
+	}
+}
 ?>
 
 <!DOCTYPE html>
@@ -24,28 +44,58 @@
 	<p>次のフォームに必要事項をご記入ください。</p>
 	<form action="" method="post" enctype="multipart/form-data">
 		<dl>
+
 			<dt>
 				ニックネーム<span class="required">必須</span>
 			</dt>
 			<dd>
-				<input type="text" name="name" size="35" maxlength="255" value="" />
+				<input type="text" name="name" size="35" maxlength="255"
+				value="<?php
+					// print(htmlspecialchars($_POST['name'], ENT_QUOTES))で入力した名前を維持できる
+					print(htmlspecialchars($_POST['name'], ENT_QUOTES));
+				?>" />
 				<?php if ($error['name'] === 'blank'): ?>
 					<p class="error">* ニックネームを入力してください</p>
 				<?php endif; ?>
 			</dd>
-			<dt>メールアドレス<span class="required">必須</span></dt>
-			<dd>
-				<input type="text" name="email" size="35" maxlength="255" value="" />
-			<dt>パスワード<span class="required">必須</span></dt>
-			<dd>
-				<input type="password" name="password" size="10" maxlength="20" value="" />
+
+			<dt>
+				メールアドレス<span class="required">必須</span>
+			</dt>
+				<input type="text" name="email" size="35" maxlength="255"
+				value="<?php
+					print(htmlspecialchars($_POST['email'], ENT_QUOTES));
+				?>" />
+				<?php if ($error['email'] === 'blank'): ?>
+					<p class="error">* メールアドレスを入力してください</p>
+				<?php endif; ?>
 			</dd>
+
+			<dt>
+				パスワード<span class="required">必須</span>
+			</dt>
+			<dd>
+				<input type="password" name="password" size="10" maxlength="20"
+				value="<?php
+					print(htmlspecialchars($_POST['password'], ENT_QUOTES));
+				?>" />
+				<?php if ($error['password'] === 'length'): ?>
+					<p class="error">* パスワードは4文字以上で入力してください</p>
+				<?php endif; ?>
+				<?php if ($error['password'] === 'blank'): ?>
+					<p class="error">* パスワードを入力してください</p>
+				<?php endif; ?>
+			</dd>
+
 			<dt>写真など</dt>
 			<dd>
-						<input type="file" name="image" size="35" value="test"  />
-					</dd>
+				<input type="file" name="image" size="35" value="test"  />
+			</dd>
+
 		</dl>
-		<div><input type="submit" value="入力内容を確認する" /></div>
+		<div>
+			<input type="submit" value="入力内容を確認する" />
+		</div>
 	</form>
 </div>
 </body>
